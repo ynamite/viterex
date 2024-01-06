27bbff05c5923ea890b07b171abe2c23238a6613
Yves Torres <studio@massif.ch>
2024-01-06 19:13:18 +0100

    updated vite config

--- setup/vite.config.js
+++ setup/vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import usePHP from 'vite-plugin-php'
 
 export default defineConfig({
-  plugins: [usePHP()]
+  plugins: [
+    usePHP({
+      // binary: '/opt/lampp/bin/php-8.1.10',
+      entry: [
+        './public/index.php',
+        './src/addons/**/*.php',
+        './var/data/developer/**/*.php'
+      ]
+    })
+  ]
 })
