99968585c4428d511da1b79cd911cd2fe04bb5cc
Yves Torres <studio@massif.ch>
2024-01-09 14:06:30 +0100

    added vite include

--- setup/index.frontend.php
+++ setup/index.frontend.php
@@ -1,4 +1,6 @@
 <?php
+define('IS_VITE_DEVELOPMENT', true);
+include "../setup/inc.vite.php";
 
 unset($REX);
 $REX['REDAXO'] = false;
@@ -6,7 +8,7 @@ $REX['HTDOCS_PATH'] = './';
 $REX['BACKEND_FOLDER'] = 'redaxo';
 $REX['LOAD_PAGE'] = true;
 
-require __DIR__.'/../src/path_provider.php';
+require __DIR__ . '/../src/path_provider.php';
 $REX['PATH_PROVIDER'] = new app_path_provider();
 
 require $REX['PATH_PROVIDER']->core('boot.php');
