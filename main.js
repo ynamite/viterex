f88a3cf9ebdcb565e663864460031efd1cbfd335
Yves Torres <studio@massif.ch>
2024-01-09 10:55:42 +0100

    added main.js as vite entry

---
+++ main.js
@@ -0,0 +1,15 @@
+// main entry point
+// include your assets here
+
+// get styles
+import './assets/css/style.css'
+
+// get scripts
+import './assets/js/test.js'
+
+/*
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+*/
