4cb812099b4c75347891db3b8594ee2461ee04e2
ynamite <ynamite@bluewin.ch>
2024-01-29 09:22:56 +0100

    cleaned up redaxo valet driver

--- LocalValetDriver.php
+++ LocalValetDriver.php
@@ -6,19 +6,14 @@ use Valet\Drivers\LaravelValetDriver;
 class LocalValetDriver extends LaravelValetDriver
 {
 
-  public function getUrlParts($uri)
-  {
-    $uriParts = array_filter(explode('/', $uri));
-    return $uriParts;
-  }
+  protected $hasPublicDirectory = false;
 
   /**
-   * Determine if the driver serves the request.
+   * Emulate the REDAXO CMS .htaccess file
    */
-  public function serves(string $sitePath, string $siteName, string $uri): bool
+  public function emulateRedaxoHtaccess($uri)
   {
-
-    $uriParts = $this->getUrlParts($uri);
+    $uriParts = array_filter(explode('/', $uri));
     switch ($uriParts[1]) {
       case 'sitemap.xml':
         $_REQUEST['rex_yrewrite_func'] = 'sitemap';
@@ -28,16 +23,88 @@ class LocalValetDriver extends LaravelValetDriver
         break;
       case 'mediatypes':
       case 'media':
-        $mmType = $uriParts[2];
-        $mmFile = $uriParts[3];
+        $mmType = isset($uriParts[2]) ? $uriParts[2] : null;
+        $mmFile = isset($uriParts[3]) ? $uriParts[3] : null;
         if ($mmType && $mmFile) {
           $_GET['rex_media_type'] = $mmType;
           $_GET['rex_media_file'] = $mmFile;
         }
         break;
     }
+  }
 
-    return file_exists($sitePath . '/public/index.php') &&
-      file_exists($sitePath . '/artisan');
+  /**
+   * Get the path to the application's front controller.
+   */
+  public function getPublicPath($sitePath)
+  {
+    $publicPath = $sitePath . '/';
+    if ($this->hasPublicDirectory) {
+      $publicPath .= 'public/';
+    }
+    return $publicPath;
+  }
+
+  /**
+   * Determine if the driver serves the request.
+   */
+  public function serves(string $sitePath, string $siteName, string $uri): bool
+  {
+    $this->hasPublicDirectory = is_dir($sitePath . '/public/');
+    return ($this->hasPublicDirectory && file_exists($sitePath . '/public/redaxo/index.php')) || file_exists($sitePath . '/redaxo/index.php');
+  }
+
+  /**
+   * Take any steps necessary before loading the front controller for this driver.
+   */
+  public function beforeLoading(string $sitePath, string $siteName, string $uri): void
+  {
+    $this->emulateRedaxoHtaccess($uri);
+  }
+
+  /**
+   * Determine if the incoming request is for a static file.
+   */
+  public function isStaticFile(string $sitePath, string $siteName, string $uri)/*: string|false */
+  {
+    $publicPath = $this->getPublicPath($sitePath) . trim($uri, '/');
+    if ($this->isActualFile($publicPath)) {
+      return $publicPath;
+    } elseif (file_exists($publicPath . '/index.html')) {
+      return $publicPath . '/index.html';
+    }
+
+    return false;
+  }
+
+  /**
+   * Get the fully resolved path to the application's front controller.
+   */
+  public function frontControllerPath(string $sitePath, string $siteName, string $uri): ?string
+  {
+    $_SERVER['PHP_SELF'] = $uri;
+    $_SERVER['SERVER_ADDR'] = '127.0.0.1';
+    $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+
+    $docRoot = $this->getPublicPath($sitePath);
+    $uri = rtrim($uri, '/');
+
+    $candidates = [
+      $docRoot . $uri,
+      $docRoot . $uri . '/index.php',
+      $docRoot . '/index.php',
+    ];
+
+    foreach ($candidates as $candidate) {
+      if ($this->isActualFile($candidate)) {
+        $_SERVER['SCRIPT_FILENAME'] = $candidate;
+        $_SERVER['SCRIPT_NAME'] = str_replace($docRoot, '', $candidate);
+        $_SERVER['DOCUMENT_ROOT'] = $docRoot;
+
+        return $candidate;
+      }
+    }
+
+    return null;
   }
 }
