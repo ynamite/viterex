f1e2ee0d661ad645c36154e107412d08ac7ce7b0
ynamite <ynamite@bluewin.ch>
2024-02-27 09:40:36 +0100

    remove tailwind defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,6 @@ module.exports = {
         primary: '#2F5A33',
         accent: '#9F8158',
       },
-      fontSize: {
-        base: ['1.25rem', { lineHeight: '1.3' }]
-      },
-      padding: {
-        '30px': '30px'
-      }
     }
   },
   plugins: [addDynamicIconSelectors()]
