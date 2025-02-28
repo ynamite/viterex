7a41e26d1f2626581f4637594b8c270d64cf5d5d
ynamite <ynamite@bluewin.ch>
2025-02-28 13:34:27 +0100

    feat: add more dirs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './assets/js/**/*.js',
-    './assets/css/**/*.css',
-    './src/fragments/**/*.php',
-    './src/templates/**/*.php',
-    './src/modules/**/*.php'
+      './assets/js/**/*.js',
+      './assets/css/**/*.css',
+      './src/fragments/**/*.php',
+      './src/templates/**/*.php',
+      './src/modules/**/*.php',
+      './src/addons/project/**/*.php'
   ],
   theme: {
     fontFamily: {
