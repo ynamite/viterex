fd1f167ef3829fa7713f8b13b86f32e07c182bc7
ynamite <ynamite@bluewin.ch>
2025-03-01 20:54:49 +0100

    feat: added fluid tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
+
 module.exports = {
-  content: [
+  content: {
+    files: [
       './assets/js/**/*.js',
       './assets/css/**/*.css',
       './src/fragments/**/*.php',
       './src/templates/**/*.php',
       './src/modules/**/*.php',
       './src/addons/project/**/*.php'
-  ],
+    ],
+    extract
+  },
   theme: {
+        screens,
+    fontSize,
+
     fontFamily: {
       roboto: ['Roboto', 'sans-serif']
     }
   },
-  plugins: []
+  plugins: [
+    fluid({
+      checkSC144: false // default: true
+    })
+  ]
 }
